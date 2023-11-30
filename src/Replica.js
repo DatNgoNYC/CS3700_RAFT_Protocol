4635f4357b4ada53b7a0fe1e38475011a001776e
Dat Ngo <datngonyc@gmail.com>
2023-11-29 20:09:09 -0500

    First outline of new state pattern design.

--- src/Replica.js
+++ src/Replica.js
@@ -1,49 +1,104 @@
-const { Socket, createSocket } = require('dgram');
-const { Follower } = require('./states/Follower');
-
-/**
- * The Replica class represents a replica in our key-value data store.
- *
- * We are using the state design pattern and so we designate the Replica to act as the "context" as well and allows our state implementations to execute based on the curent "context"/"state" of the replica.
- * @param {string} port - The port our replica should send messages to on localhost.
- * @param {string} id = The id of our replica. It is a string in hexideximal form so exactly four characters, 0-9, a-z.
- * @param {(string)[]} others - The is list of other replicas, by id, in our cluster.
- */
-class Replica {
-  constructor(port, id, others) {
-    /** @type {State.js} The state of our replica. */
-    this.current_state = new Follower(this);
-    /** @type {number}  The port we should send messages to. */
-    this.port = Number(port);
-    /** @type {string} - The id of our replica. It is a string  */
-    this.id = id;
-    /** @type {string[]} - The id of our replica. It is a string  */
-    this.others = others;
-    /** @type {Socket} - Our replica's socket we'll use to communicate across the transport layer. */
-    this.socket = createSocket('udp4');
-    this.socket.bind(0, '0.0.0.0', () => {
-      this.socket.send()
-
-      // create our message as a json object
-      // convert it to string
-      // argument for this.socket.send();
-    });
-
-    /* _______________________ RAFT PROTOCOL PROPERTIES _______________________ */
-    /** @type {number} term - The current term of this replica. */
-    this.term = 0;
-  }
-
-  start() {
-    this.current_state.run();
-  }
-
-  /** @param {State.js} state - The new state of our replica. */
-  changeState(state) {
-    this.current_state = state;
-  }
+// Base State Class (Pseudo-abstract)
+function BaseRaftState(replica) {
+  // Reference to the replica
+  this.replica = replica;
 }
 
-module.exports = {
-  Replica,
+// Run method should be abstract, but JavaScript doesn't have explicit support for abstract methods.
+BaseRaftState.prototype.run = function () {
+  throw new Error("Abstract method run must be overridden");
 };
+
+// Follower State
+function FollowerState(replica) {
+  // Call the constructor of the base class
+  BaseRaftState.call(this, replica);
+}
+
+// Set up prototype inheritance for FollowerState
+FollowerState.prototype = Object.create(BaseRaftState.prototype);
+// Fix the constructor property after setting up the prototype chain
+FollowerState.prototype.constructor = FollowerState;
+
+// Implement the run method specific to the Follower state
+FollowerState.prototype.run = function () {
+  console.log("Follower State");
+  // Access common properties and methods from the Replica context
+  this.replica.commonMethod();
+};
+
+// Candidate State
+function CandidateState(replica) {
+  // Call the constructor of the base class
+  BaseRaftState.call(this, replica);
+  // Additional property specific to the Candidate state
+  this.candidateProperty = "Candidate-specific property";
+}
+
+// Set up prototype inheritance for CandidateState
+CandidateState.prototype = Object.create(BaseRaftState.prototype);
+// Fix the constructor property after setting up the prototype chain
+CandidateState.prototype.constructor = CandidateState;
+
+// Implement the run method specific to the Candidate state
+CandidateState.prototype.run = function () {
+  console.log("Candidate State");
+  // Access common properties and methods from the Replica context
+  this.replica.commonMethod();
+  // Access state-specific property
+  console.log("Candidate Property:", this.candidateProperty);
+};
+
+// Leader State
+function LeaderState(replica) {
+  // Call the constructor of the base class
+  BaseRaftState.call(this, replica);
+  // Additional method specific to the Leader state
+  this.leaderMethod = function () {
+      console.log("Leader-specific method");
+      // Leader-specific logic
+  };
+}
+
+// Set up prototype inheritance for LeaderState
+LeaderState.prototype = Object.create(BaseRaftState.prototype);
+// Fix the constructor property after setting up the prototype chain
+LeaderState.prototype.constructor = LeaderState;
+
+// Implement the run method specific to the Leader state
+LeaderState.prototype.run = function () {
+  console.log("Leader State");
+  // Access common properties and methods from the Replica context
+  this.replica.commonMethod();
+  // Access state-specific method
+  this.leaderMethod();
+};
+
+// Replica Class (Context)
+function Replica() {
+  // Common properties shared among states
+  this.term = 0;
+  this.logOfAppendEntries = [];
+
+  // Common method shared among states
+  this.commonMethod = function () {
+      console.log("Common method");
+      // Common logic
+  };
+
+  // Set the initial state to Follower
+  this.state = new FollowerState(this);
+}
+
+// Method to run the current state's logic
+Replica.prototype.run = function () {
+  this.state.run();
+};
+
+// Example usage
+const replica = new Replica();
+replica.run(); // Outputs: Follower State
+
+// Transition to Candidate State
+replica.state = new CandidateState(replica);
+replica.run(); // Outputs: Candidate State
