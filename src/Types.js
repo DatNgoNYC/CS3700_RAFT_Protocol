0bf273e09c4de6a2ad553bc73264c70507045256
Dat Ngo <datngonyc@gmail.com>
2023-12-06 18:18:03 -0500

    Merge origin/main into target main.

--- src/Types.js
+++
@@ -1,7 +0,0 @@
-/**
- * @typedef get - The syntax for a get request
- * @prop {string} src
- * @prop {string} dst
- * @prop {'update' | 'withdraw' | 'dump' | 'data' | 'no route' | 'table' | 'handshake'} type
- * @prop {any} msg
- */
---
+++ src/states/CandidateState.js
@@ -0,0 +1,34 @@
+const BaseRaftState = require("./BaseRaftState");
+
+/**
+ * Candidate state class.
+ * @class
+ * @extends BaseRaftState
+ */
+class CandidateState {
+  /**
+   * Creates an instance of CandidateState.
+   * @param {Replica} replica - The Replica instance.
+   */
+  constructor(replica) {
+    // Call the constructor of the base class
+    BaseRaftState.call(this, replica);
+    // Additional property specific to the Candidate state
+    this.candidateProperty = 'Candidate-specific property';
+  }
+
+  /**
+   * Implement the run method specific to the Candidate state.
+   */
+  run() {
+    console.log('Candidate State');
+    // Access common properties and methods from the Replica context
+    this.replica.commonMethod();
+    // Access state-specific property
+    console.log('Candidate Property:', this.candidateProperty);
+  }
+}
+
+module.exports = {
+  CandidateState
+}
